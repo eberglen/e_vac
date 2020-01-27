c39600fbbc2e8708eace7358601ebca05da9d8dd
eberglen <60251884+eberglen@users.noreply.github.com>
2020-01-27 17:00:43 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Maps')
+      ),
+      body: Center(
+          child: Container(
+              height: 400.0,
+              width: MediaQuery.of(context).size.width,
+              child: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(40.7128,74.0060),
+                    zoom: 12.0
+                ),
+              )
+          )
+      ),
+    );
+  }
+}
