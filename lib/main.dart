4b2625129a1d8f5ffca83c6b1603124ea07e7c53
eberglen <60251884+eberglen@users.noreply.github.com>
2020-01-27 18:54:53 +0800

    get current location

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:e_vac/services/location_service.dart';
+import 'package:provider/provider.dart';
+import 'data_models/user_location.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+    return StreamProvider<UserLocation>(
+      // ignore: deprecated_member_use
+      builder: (context) => LocationService().locationStream,
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: Scaffold(
+            body: HomeView(),
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
 
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var userLocation = Provider.of<UserLocation>(context);
     return Scaffold(
       appBar: AppBar(
-          title: Text('Maps')
+          title: Text('E-Vac')
       ),
       body: Center(
           child: Container(
@@ -31,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
               width: MediaQuery.of(context).size.width,
               child: GoogleMap(
                 initialCameraPosition: CameraPosition(
-                    target: LatLng(40.7128,74.0060),
+                    target: LatLng(14.5764,121.0851),
                     zoom: 12.0
                 ),
               )
